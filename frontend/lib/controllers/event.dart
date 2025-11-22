8fbd0157e285239d8465cbba94d565561520b46e
Nane <kds.napha@gmail.com>
2025-11-23 03:42:30 +0900

    rename meeting to event

--- frontend/lib/controllers/event.dart
+++ frontend/lib/controllers/event.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../models/meeting.dart';
+import '../models/event.dart';
 
 // Use Notifier instead of StateNotifier
-class MeetingController extends Notifier<List<Meeting>> {
+class MeetingController extends Notifier<List<Event>> {
   @override
-  List<Meeting> build() {
+  List<Event> build() {
     // Initial state
     final today = DateTime.now();
     final startTime = DateTime(today.year, today.month, today.day, 9);
     final endTime = startTime.add(const Duration(hours: 2));
-    return [Meeting('Conference', startTime, endTime, const Color(0xFF0F8644), false)];
+    return [Event('Conference', startTime, endTime, const Color(0xFF0F8644), false)];
   }
 
-  // Add a new meeting
-  void addMeeting(Meeting meeting) {
+  void addEvent(Event meeting) {
     state = [...state, meeting];
   }
 }
 
 // Provider
-final meetingProvider = NotifierProvider<MeetingController, List<Meeting>>(() => MeetingController());
+final eventProvider = NotifierProvider<MeetingController, List<Event>>(() => MeetingController());
--- frontend/lib/views/home.dart
+++ frontend/lib/views/home.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import '../controllers/home.dart';
-import '../controllers/meeting.dart';
-import '../models/meeting.dart';
+import '../controllers/event.dart';
+import '../models/event.dart';
 
 class Home extends ConsumerWidget {
   const Home({super.key});
@@ -11,45 +11,68 @@ class Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final homeController = ref.watch(homeControllerProvider);
-    final meetings = ref.watch(meetingProvider);
+    final events = ref.watch(eventProvider);
 
     return Scaffold(
       body: SfCalendar(
+        //controller: calendarController,
+        //showNavigationArrow: model.isWebFullView,
         allowedViews: homeController.allowedViews,
-        dataSource: MeetingDataSource(meetings),
+        showDatePickerButton: true,
+        //scheduleViewMonthHeaderBuilder: scheduleViewBuilder,
+        dataSource: EventDataSource(events),
+        //onTap: calendarTapCallback,
+        //onViewChanged: viewChangedCallback,
+        initialDisplayDate: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day),
         monthViewSettings: const MonthViewSettings(appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        timeSlotViewSettings: const TimeSlotViewSettings(minimumAppointmentDuration: Duration(minutes: 60)),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final now = DateTime.now();
-          ref.read(meetingProvider.notifier).addMeeting(Meeting('New Meeting', now, now.add(const Duration(hours: 1)), Colors.blue, false));
-        },
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     final now = DateTime.now();
+      //     ref.read(eventProvider.notifier).addEvent(Event('New Meeting', now, now.add(const Duration(hours: 1)), Colors.blue, false));
+      //   },
+      //   child: const Icon(Icons.add),
+      // ),
     );
+
+    // return Scaffold(
+    //   body: SfCalendar(
+    //     allowedViews: homeController.allowedViews,
+    //     dataSource: EventDataSource(events),
+    //     monthViewSettings: const MonthViewSettings(appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: () {
+    //       final now = DateTime.now();
+    //       ref.read(eventProvider.notifier).addEvent(Event('New Meeting', now, now.add(const Duration(hours: 1)), Colors.blue, false));
+    //     },
+    //     child: const Icon(Icons.add),
+    //   ),
+    // );
   }
 }
 
 /// Calendar data source mapping
-class MeetingDataSource extends CalendarDataSource {
-  MeetingDataSource(List<Meeting> source) {
+class EventDataSource extends CalendarDataSource {
+  EventDataSource(List<Event> source) {
     appointments = source;
   }
 
   @override
-  DateTime getStartTime(int index) => _getMeeting(index).from;
+  DateTime getStartTime(int index) => _getEvent(index).from;
 
   @override
-  DateTime getEndTime(int index) => _getMeeting(index).to;
+  DateTime getEndTime(int index) => _getEvent(index).to;
 
   @override
-  String getSubject(int index) => _getMeeting(index).eventName;
+  String getSubject(int index) => _getEvent(index).name;
 
   @override
-  Color getColor(int index) => _getMeeting(index).background;
+  Color getColor(int index) => _getEvent(index).background;
 
   @override
-  bool isAllDay(int index) => _getMeeting(index).isAllDay;
+  bool isAllDay(int index) => _getEvent(index).isAllDay;
 
-  Meeting _getMeeting(int index) => appointments![index] as Meeting;
+  Event _getEvent(int index) => appointments![index] as Event;
 }

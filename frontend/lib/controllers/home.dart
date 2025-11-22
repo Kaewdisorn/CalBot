e65783226764fd08d543050c25f48af6cd3cf392
Nane <kds.napha@gmail.com>
2025-11-23 07:39:28 +0900

    Added event when tap calendar

--- frontend/lib/controllers/home.dart
+++ frontend/lib/controllers/home.dart
@@ -1,5 +1,14 @@
+import 'dart:ui';
+
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import '../models/schedule.dart';
 
 class HomeController {
   final allowedViews = <CalendarView>[CalendarView.day, CalendarView.week, CalendarView.month, CalendarView.schedule];
+
+  /// Create a new Schedule object
+  Schedule createSchedule(String title, DateTime start, DateTime end, {Color color = const Color(0xFF0F8644), bool isAllDay = false}) {
+    return Schedule(title, start, end, color, isAllDay);
+  }
 }

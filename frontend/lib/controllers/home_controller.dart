3cfe0dcdca993a033cbd81bb7a593932f68e1c16
Nane <kds.napha@gmail.com>
2025-11-30 11:57:20 +0900

    move scheduleList to controller

--- frontend/lib/controllers/home_controller.dart
+++ frontend/lib/controllers/home_controller.dart
@@ -1,4 +1,7 @@
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import '../models/schedule_model.dart';
 
 class HomeController {
   final List<CalendarView> allowedViews = <CalendarView>[
@@ -11,4 +14,31 @@ class HomeController {
     CalendarView.month,
     CalendarView.schedule,
   ];
+
+  // observable schedule list retained across rebuilds
+  final RxList<ScheduleModel> scheduleList = <ScheduleModel>[
+    ScheduleModel.fromJson({
+      'id': '1',
+      'title': 'Team Meeting',
+      'start': DateTime.now().add(Duration(hours: 1)).toIso8601String(),
+      'end': DateTime.now().add(Duration(hours: 2)).toIso8601String(),
+      'recurrenceRule': 'FREQ=DAILY;INTERVAL=1;COUNT=10',
+      'exceptionDateList': [DateTime(2025, 12, 05).toIso8601String()],
+      'colorValue': 0xFF42A5F5,
+    }),
+    ScheduleModel.fromJson({
+      'id': '2',
+      'title': 'Client Call',
+      'start': DateTime.now().add(Duration(days: 1, hours: 3)).toIso8601String(),
+      'end': DateTime.now().add(Duration(days: 1, hours: 4)).toIso8601String(),
+      'colorValue': 0xFF66BB6A,
+    }),
+    ScheduleModel.fromJson({
+      'id': '3',
+      'title': 'Shopping',
+      'start': DateTime.now().add(Duration(days: 1, hours: 3)).toIso8601String(),
+      'end': DateTime.now().add(Duration(days: 2, hours: 4)).toIso8601String(),
+      'colorValue': 0xFF66BB6A,
+    }),
+  ].obs;
 }
--- frontend/lib/views/home_view.dart
+++ frontend/lib/views/home_view.dart
@@ -11,33 +11,6 @@ import 'widgets/settings_drawer.dart';
 class HomeView extends StatelessWidget {
   final homeController = Get.find<HomeController>();
 
-  // observable schedule list retained across rebuilds
-  final RxList<ScheduleModel> scheduleList = <ScheduleModel>[
-    ScheduleModel.fromJson({
-      'id': '1',
-      'title': 'Team Meeting',
-      'start': DateTime.now().add(Duration(hours: 1)).toIso8601String(),
-      'end': DateTime.now().add(Duration(hours: 2)).toIso8601String(),
-      'recurrenceRule': 'FREQ=DAILY;INTERVAL=1;COUNT=10',
-      'exceptionDateList': [DateTime(2025, 12, 05).toIso8601String()],
-      'colorValue': 0xFF42A5F5,
-    }),
-    ScheduleModel.fromJson({
-      'id': '2',
-      'title': 'Client Call',
-      'start': DateTime.now().add(Duration(days: 1, hours: 3)).toIso8601String(),
-      'end': DateTime.now().add(Duration(days: 1, hours: 4)).toIso8601String(),
-      'colorValue': 0xFF66BB6A,
-    }),
-    ScheduleModel.fromJson({
-      'id': '3',
-      'title': 'Shopping',
-      'start': DateTime.now().add(Duration(days: 1, hours: 3)).toIso8601String(),
-      'end': DateTime.now().add(Duration(days: 2, hours: 4)).toIso8601String(),
-      'colorValue': 0xFF66BB6A,
-    }),
-  ].obs;
-
   HomeView({super.key});
 
   @override
@@ -49,8 +22,7 @@ class HomeView extends StatelessWidget {
 
       body: Obx(() {
         // recreate data source from current schedule list so calendar updates
-        final dataSource = ScheduleDataSource(scheduleList.toList());
-
+        final dataSource = ScheduleDataSource(homeController.scheduleList.toList());
         return SfCalendar(
           view: CalendarView.month,
           showDatePickerButton: true,
@@ -98,7 +70,7 @@ class HomeView extends StatelessWidget {
                 final newId = DateTime.now().millisecondsSinceEpoch.toString();
                 final newModel = ScheduleModel(id: newId, title: result, start: tapped, end: tapped.add(const Duration(hours: 1)));
 
-                scheduleList.add(newModel);
+                homeController.scheduleList.add(newModel);
               }
             }
           },

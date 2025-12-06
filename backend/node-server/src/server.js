5382bfc49041301402458c5b1c0297108f1fce0d
Nane <kds.napha@gmail.com>
2025-12-07 03:34:22 +0900

    change .yml dir

--- backend/node-server/src/server.js
+++ backend/node-server/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const envPath = path.resolve(__dirname, '../.env');
+const envPath = path.resolve(__dirname, '../../../.env');
 require('dotenv').config({ path: envPath });
 
 const apiRoutes = require('./routes/api.js');

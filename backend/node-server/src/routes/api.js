452550c347bdf7921f5f0335d26084f34714255d
Nane <kds.napha@gmail.com>
2025-12-08 04:47:41 +0900

    added auth api

--- backend/node-server/src/routes/api.js
+++ backend/node-server/src/routes/api.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const healthRoutes = require('./health/health.js');
 const scheduleRoutes = require('./schedule/schedule.js');
+const authRoutes = require('./auth/auth.js');
 
+router.use('/auth', authRoutes);
 router.use('/health', healthRoutes);
 router.use('/schedules', scheduleRoutes);
 
---
+++ backend/node-server/src/routes/auth/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const authController = require('./../../controllers/auth.js');
+
+
+router.post('/login', //loginValidation, 
+    authController.login);
+
+module.exports = router;

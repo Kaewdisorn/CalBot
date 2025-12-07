956cca0b6628b9d1dd55c2e0711bf9fb5d996940
Nane <kds.napha@gmail.com>
2025-12-08 08:08:06 +0900

    create api response tool

---
+++ backend/node-server/src/utils/response.js
@@ -0,0 +1,12 @@
+
+const apiRes = (res, statusCode, message, data) => {
+    return res.status(statusCode).json({
+        status: statusCode,
+        message: message,
+        data: data
+    });
+};
+
+module.exports = {
+    apiRes
+};

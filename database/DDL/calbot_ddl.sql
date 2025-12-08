b6f410d7bb1558e52cd6e8754541e857387a2e8f
Nane <kds.napha@gmail.com>
2025-12-08 09:48:16 +0900

    edit user DDL

--- database/DDL/calbot_ddl.sql
+++ database/DDL/calbot_ddl.sql
@@ -3,11 +3,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- CREATE users table
 CREATE TABLE v1.users (
-    gid             UUID NOT NULL,   
-    uid             UUID NOT null PRIMARY KEY,
-    properties      JSONB DEFAULT '{}'::jsonb,                 
-    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	gid uuid NOT NULL,
+	uid uuid NOT NULL,
+	properties jsonb DEFAULT '{}'::jsonb NULL,
+	created_at timestamptz DEFAULT CURRENT_TIMESTAMP NULL,
+	updated_at timestamptz DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT gid_uid_same CHECK ((gid = uid)),
+	CONSTRAINT users_pkey PRIMARY KEY (gid, uid)
 );
 
 -- CREATE schedules table

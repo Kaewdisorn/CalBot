0efc7ab312ab56c88606555acea83b28abf99447
Nane <kds.napha@gmail.com>
2025-12-18 12:54:55 +0900

    created ScheduleDataSource

---
+++ frontend/lib/data/datasource/schedule_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import '../../models/schedule_model.dart';
+
+class ScheduleDataSource extends CalendarDataSource {
+  ScheduleDataSource(List<ScheduleModel> models) {
+    appointments = models.map((e) => e.toCalendarAppointment()).toList();
+  }
+}

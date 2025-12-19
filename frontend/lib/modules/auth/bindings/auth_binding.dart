b87a323685101c9b6faeae3eb523028ae44a4332
Nane <kds.napha@gmail.com>
2025-12-19 09:06:19 +0900

    AuthRepository binding

--- frontend/lib/modules/auth/bindings/auth_binding.dart
+++ frontend/lib/modules/auth/bindings/auth_binding.dart
@@ -1,12 +1,12 @@
 import 'package:get/get.dart';
-
-import '../controllers/auth_controller.dart';
+import 'package:halulu/data/repositories/auth_repository.dart';
+import 'package:halulu/modules/auth/controllers/auth_controller.dart';
 
 class AuthBinding extends Bindings {
   @override
   void dependencies() {
     // Get.lazyPut<AuthProvider>(() => AuthProvider());
-    // Get.lazyPut<AuthRepository>(() => AuthRepository());
+    Get.lazyPut<AuthRepository>(() => AuthRepository());
     Get.put<AuthController>(AuthController(), permanent: true);
   }
 }

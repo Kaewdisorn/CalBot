831b57533a9b1df5911d81a26c460f97e0cf3fff
Nane <kds.napha@gmail.com>
2025-12-29 18:51:51 +0900

    added SfCalendar widget

--- frontend/lib/modules/home/views/home_view.dart
+++ frontend/lib/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:halulu/core/widgets/custom_appbar.dart';
 import 'package:halulu/modules/home/controllers/home_controller.dart';
 import 'package:halulu/modules/home/views/drawer_view.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class HomeView extends GetView<HomeController> {
   static const logoImgPath = 'assets/images/halulu_128x128.png';
@@ -15,6 +16,14 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       appBar: CustomAppBar(toolbarHeight: 60, titleText: headerTitle, logoAsset: logoImgPath, appbarColor: null, welcomeUsername: controller.displayName),
       endDrawer: const DrawerView(),
+      body: SfCalendar(
+        view: CalendarView.month,
+        allowedViews: [CalendarView.month, CalendarView.schedule],
+        monthViewSettings: const MonthViewSettings(appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        onTap: (CalendarTapDetails details) {
+          debugPrint('Tapped on: ${details.date}, Appointments: ${details.appointments}');
+        },
+      ),
     );
   }
 }
